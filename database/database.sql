0aa4bd6a388542ed6d80657f69ef914d9095f473
Andres1069 <raulandresgonzalezcifuentes@gmail.com>
2025-08-27 20:13:49 -0500

    Base de datos creada, hacer Pedidos relacionado con usuarios

--- database/database.sql
+++ database/database.sql
@@ -3,15 +3,38 @@ CREATE DATABASE IF NOT EXISTS tienda;
 USE tienda;
 
 CREATE TABLE IF NOT EXISTS t_usuarios(
-    id_usuario INT(11) AUTO_INCREMENT NOT NULL,
-    nombre VARCHAR(100) NOT NULL,
-    apellidos VARCHAR(100) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    rol VARCHAR(20),
-    imagen VARCHAR(255),
-    CONSTRAINT pk_usuarios PRIMARY KEY(id_usuario),
-    CONSTRAINT uq_email UNIQUE(email)
-)ENGINE=InnoDB;
+    id_usuario      int(11) auto_increment  NOT NULL,
+    nombre          varchar(100)            NOT NULL,
+    apellidos       varchar(100)            NOT NULL,
+    email           varchar(200)            NOT NULL,
+    password        varchar(255)            NOT NULL,
+    rol             varchar(20),
+    imagen          varchar(255),
+    CONSTRAINT      pk_usuario      PRIMARY KEY(id_usuario),
+    CONSTRAINT      uq_email        UNIQUE(email)
+)ENGINE=InnoDb;
 
-INSERT INTO t_usuarios VALUES (NULL , 'admin', 'admin', 'admin@admin.com', '1234', 'admin', 'imagen');
+INSERT INTO t_usuarios VALUES(NULL, 'admin', 'admin','admin@admin.com','1234','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria        int(11) auto_increment  NOT NULL,
+    nombre_categoria    varchar(100)            NOT NULL,
+    CONSTRAINT pk_categoria PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
+
+INSERT INTO t_categorias VALUE(NULL, 'Construcccion'),(null,'Electricos'),(null,'Pisos');
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto         int(11) auto_increment  NOT NULL,
+    nombre_producto     varchar(100)            NOT NULL,
+    precio              float(100,2)            NOT NULL,
+    stock               int(100)                NOT NULL,
+    oferta              varchar(50)             NOT NULL,
+    fecha               date                    NOT NULL,
+    imagen              varchar(255),
+    id_categoria        int(11)                 NOT NULL,
+    CONSTRAINT          pk_producto             PRIMARY KEY(id_producto),
+    CONSTRAINT          fk_producto_categoria   FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDb;
+
+//Pedidos relacionado con usuarios
